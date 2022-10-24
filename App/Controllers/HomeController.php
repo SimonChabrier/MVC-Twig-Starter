e38d495483d0de84ffb76a2a34a0ab8b9490ac7d
Simon Chabrier <simonchabrier@gmail.com>
2022-10-24 07:53:35 +0200

    add json response

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 use App\Models\User;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
